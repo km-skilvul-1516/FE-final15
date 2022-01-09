697135735acf33d5034cdc557c8e8654acd6bc9c
frensiatanagaanaclaudia <frensiaskilvul@gmail.com>
2022-01-09 19:12:14 +0800

    Tambahan component dan package json

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import Login from './components/login';
 import Register from './components/register'
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import Home from './components/home/Home';
+import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import About from './Components/About';
+import Testi from './Components/Testi';
+import Product from './Components/Products'
+import Contact from './Components/Contact';
+import Feature from './Components/Feature';
+import Reason from './Components/Reason';
+import Footer from './Components/Footer';
+
+
 
 function App() {
   return (
@@ -11,6 +22,15 @@ function App() {
         <Route exact path="/" component={Home}/>
         <Route exact path="/login" component={Login}/>
         <Route exact path = "/register" component={Register}/>
+        <Navbar/>
+        <Header/>
+        <Feature/>
+        <Reason/>
+        <Product/>
+        <About/>
+        <Testi/>
+        <Contact/> 
+        <Footer/>
       </div>
 
     </Router>
