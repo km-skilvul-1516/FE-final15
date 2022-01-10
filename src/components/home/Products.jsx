2871e43e747802d9f417ab8690b76a09536f20f7
nurrahmiardani <73676678+nurrahmiardani@users.noreply.github.com>
2022-01-10 14:01:49 +0700

    index js

---
+++ src/components/home/Products.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Productbox from './Productbox';
+import logo1 from '../../images/logo1.png';
+function Products() {
+    return (
+        <div id='products'>
+            <h1>Layanan Konseling</h1>
+                <div className='a-container'>
+                <Productbox image={logo1} title="Reguler"  />
+               <br/>
+                <Productbox image={logo1} title="Premium" />
+                <Productbox image={logo1} title="Platinum" />
+            </div>
+        </div>
+    )
+}
+
+export default Products;
