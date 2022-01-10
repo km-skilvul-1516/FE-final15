326c0b23bf5b4abc0431cd7b7e0978266c9596f7
frensiatanagaanaclaudia <frensiaskilvul@gmail.com>
2022-01-10 23:35:56 +0800

    Fetch data dari BE untuk layanan dan Testimoni

--- src/components/layanan/Layanan.jsx
+++ src/components/layanan/Layanan.jsx
@@ -1,18 +1,84 @@
-import React from 'react';
-import Productbox from '../Productbox';
-import logo1 from '../../images/logo1.png';
+// import React from 'react';
+// import Productbox from '../Productbox';
+// import logo1 from '../../images/logo1.png';
+// import Navbar from '../Navbar';
+// function Layanan() {
+//     return (
+        // <div id='products'>
+        //     <Navbar/>
+        //     <h1>Layanan Konseling</h1>
+        //     <div className='a-container'>
+        //         <Productbox image={logo1} title="Reguler"/>
+        //         <Productbox image={logo1} title="Silver"/>
+        //         <Productbox image={logo1} title="Platinum"/>
+        //     </div>
+        // </div>    )}
+
+
+// export default Layanan;
+
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import Navbar from '../Navbar';
+
+
 function Layanan() {
+    const [APIData, setAPIData] = useState([]);
+    useEffect(() => {
+        axios.get("https://teslah-final.herokuapp.com/layanan/getAllLayanan")
+            .then((response) => {
+                console.log('ini dari useeffect array',response.data.result)
+                console.log('ini dari useeffect obj',response.data)
+                setAPIData(response.data.result);
+            })
+    },[])
+    console.log(APIData)
+
     return (
-        <div id='products'>
+     <div>
+            <div className="List-Paket">
             <Navbar/>
-            <h1>Layanan Konseling</h1>
-            <div className='a-container'>
-                <Productbox image={logo1} title="Reguler"/>
-                <Productbox image={logo1} title="Silver"/>
-                <Productbox image={logo1} title="Platinum"/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+
+                <h2>Paket Layanan</h2>
             </div>
-        </div>    )}
+            <div className="item">
+            {
+            !APIData ? null
+            :
+            APIData.map((post) =>
+            {
+                return(
+                    <div id='products'>
+                    <div id='testi'>
+                    <div className='a-container'>
+                    <div className='a-box'>
+                         <div className="testi">
+                         <div className='a-b-text'>
+                                {post.namaPaket}
+                                {post.deskripsiLayanan}
+                                {post.hargaLayanan}
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                
+                   ) 
+            }   
+           
+            )
+        }
+        </div>
+    </div>
+    )
+}
 
 
 export default Layanan;
